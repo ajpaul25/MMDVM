9a0e8eeb7ea79455397f71217397a65534ec0e9a
Adam Paul <adam_paul@brown.edu>
2025-08-20 13:16:04 -0400

    removed commented out code

--- SerialPort.cpp
+++ SerialPort.cpp
@@ -40,47 +40,6 @@ const uint8_t MMDVM_RSSI_DATA    = 0x09U;
 
 const uint8_t MMDVM_SEND_CWID    = 0x0AU;
 
-/*const uint8_t MMDVM_DSTAR_HEADER = 0x10U;
-const uint8_t MMDVM_DSTAR_DATA   = 0x11U;
-const uint8_t MMDVM_DSTAR_LOST   = 0x12U;
-const uint8_t MMDVM_DSTAR_EOT    = 0x13U;
-
-const uint8_t MMDVM_DMR_DATA1    = 0x18U;
-const uint8_t MMDVM_DMR_LOST1    = 0x19U;
-const uint8_t MMDVM_DMR_DATA2    = 0x1AU;
-const uint8_t MMDVM_DMR_LOST2    = 0x1BU;
-const uint8_t MMDVM_DMR_SHORTLC  = 0x1CU;
-const uint8_t MMDVM_DMR_START    = 0x1DU;
-const uint8_t MMDVM_DMR_ABORT    = 0x1EU;
-
-const uint8_t MMDVM_YSF_DATA     = 0x20U;
-const uint8_t MMDVM_YSF_LOST     = 0x21U;
-
-const uint8_t MMDVM_P25_HDR      = 0x30U;
-const uint8_t MMDVM_P25_LDU      = 0x31U;
-const uint8_t MMDVM_P25_LOST     = 0x32U;
-
-const uint8_t MMDVM_NXDN_DATA    = 0x40U;
-const uint8_t MMDVM_NXDN_LOST    = 0x41U;
-
-const uint8_t MMDVM_M17_LINK_SETUP = 0x45U;
-const uint8_t MMDVM_M17_STREAM     = 0x46U;
-const uint8_t MMDVM_M17_PACKET     = 0x47U;
-const uint8_t MMDVM_M17_LOST       = 0x48U;
-const uint8_t MMDVM_M17_EOT        = 0x49U;
-
-const uint8_t MMDVM_POCSAG_DATA  = 0x50U;*/
-
-//const uint8_t MMDVM_AX25_DATA    = 0x55U;
-
-/*const uint8_t MMDVM_FM_PARAMS1   = 0x60U;
-const uint8_t MMDVM_FM_PARAMS2   = 0x61U;
-const uint8_t MMDVM_FM_PARAMS3   = 0x62U;
-const uint8_t MMDVM_FM_PARAMS4   = 0x63U;
-const uint8_t MMDVM_FM_DATA      = 0x65U;
-const uint8_t MMDVM_FM_STATUS    = 0x66U;
-const uint8_t MMDVM_FM_EOT       = 0x67U;*/
-
 const uint8_t MMDVM_ACK          = 0x70U;
 const uint8_t MMDVM_NAK          = 0x7FU;
 
